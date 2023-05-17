e8e01422597589c22a74d4ecaf1e4f1aceeb0df2
John Aeris <barrotjohnaeris@gmail.com>
2023-05-18 01:01:19 +0800

    Updated code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.myapplication;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -19,7 +15,6 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -27,36 +22,30 @@ import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
-
 import org.jetbrains.annotations.Nullable;
-
 import java.io.File;
 import java.io.IOException;
-import java.net.InetAddress;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
-import okhttp3.OkHttp;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
-import retrofit2.http.Multipart;
-import retrofit2.http.Tag;
 
 public class MainActivity extends AppCompatActivity {
 
+    // variables declarations
     View fileContainer;
     Button uploadBtn, detectBtn;
     TextView filename;
     VideoView videoContainer;
 
+    // initial file path
     String file_path = null;
 
-    //int requestCode = 1;
+    // request codes
     private static final int PERMISSION_REQUEST_CODE = 1;
     private static final int REQUEST_GALLERY = 2;
 
@@ -72,9 +61,7 @@ public class MainActivity extends AppCompatActivity {
         filename = findViewById(R.id.filenameContainer);
         videoContainer = findViewById(R.id.videoContainer);
 
-        //uploadBtnFunction(uploadBtn);
-        //detectBtnFunction(detectBtn);
-
+        // upload button
         uploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // detect button
         detectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -106,96 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*//upload button function
-    private void uploadBtnFunction(Button button) {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(MainActivity.this, "Upload Button is working", Toast.LENGTH_SHORT).show();
-                showFileChooser();
-            }
-        });
-    }
-
-    // detect button function
-    private void detectBtnFunction(Button button) {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(MainActivity.this, "Detect Button is working", Toast.LENGTH_SHORT).show();
-                btnSendPostRequestClicked();
-            }
-        });
-    }
-
-    //file picker
-    private void showFileChooser() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("video/*");
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        try {
-            startActivityForResult(Intent.createChooser(intent, "Select a file"), requestCode);
-        } catch (Exception exception) {
-            Toast.makeText(this, "Please install a file manager", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    //file selected
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        filename = findViewById(R.id.filenameContainer);
-        videoContainer = findViewById(R.id.videoContainer);
-
-        try {
-            if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
-                Uri uri = data.getData();
-                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-                if (cursor != null && cursor.moveToFirst()) {
-                    @SuppressLint("Range") String displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                    cursor.close();
-
-                    String path = uri.getPath();
-                    File file = new File(path);
-
-                    //filename.setText(displayName);
-
-                    MediaController mediaController = new MediaController(this);
-                    mediaController.setAnchorView(videoContainer);
-                    videoContainer.setMediaController(mediaController);
-
-                    videoContainer.setVideoURI(uri);
-                    videoContainer.start();
-                }
-            }
-
-            super.onActivityResult(requestCode, resultCode, data);
-
-        } catch (Exception exception) {
-            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    // sending data in api
-    private void btnSendPostRequestClicked() {
-        ApiInterface apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
-        Call<User> call = apiInterface.getUserInformation("TestName4");
-        call.enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                Log.e(TAG, "onResponse: " + response.code());
-                Log.e(TAG, "onResponse: id : " + response.body().getId());
-                Log.e(TAG, "onResponse: name : " + response.body().getName());
-                Log.e(TAG, "onResponse: created : " + response.body().getCreated());
-            }
-
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                Log.e(TAG, "onFailure " + t.getMessage());
-                Toast.makeText(MainActivity.this, "onFailure: Error", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }*/
-
+    // POST request: sending file to API
     private void UploadFile() {
         UploadTask uploadTask = new UploadTask();
         uploadTask.execute(file_path);
@@ -203,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("File Uploaded", "File: " + file_path);
     }
 
+    // upload task class
     @SuppressLint("StaticFieldLeak")
     public class UploadTask extends AsyncTask<String, String, String>{
 
@@ -237,8 +137,6 @@ public class MainActivity extends AppCompatActivity {
             try {
                 RequestBody requestBody =  new MultipartBody.Builder().setType(MultipartBody.FORM)
                         .addFormDataPart("video", file.getName(), RequestBody.create(MediaType.parse("video/*"), file))
-                        /*.addFormDataPart("some key", "some_value")
-                        .addFormDataPart("submit", "submit")*/
                         .build();
 
                 Request request = new Request.Builder()
@@ -268,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // selecting file method
     private void filePicker() {
         Toast.makeText(MainActivity.this, "File picker call", Toast.LENGTH_SHORT).show();
         Intent openGallery = new Intent(Intent.ACTION_PICK);
@@ -275,6 +174,7 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(openGallery, REQUEST_GALLERY);
     }
 
+    // displaying the selected file
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==REQUEST_GALLERY && resultCode == Activity.RESULT_OK){
@@ -295,6 +195,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // get file path from uri method
     public String getRealPathFromUri(Uri uri, Activity activity){
         Cursor cursor = activity.getContentResolver().query(uri, null, null, null, null);
         if(cursor==null){
@@ -307,6 +208,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // request permission method
     public void requestPermission() {
         if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
             Toast.makeText(MainActivity.this, "Give permission to upload file", Toast.LENGTH_SHORT).show();
@@ -316,6 +218,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // check permission method
     private boolean checkPermission() {
         int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
         if(result == PackageManager.PERMISSION_GRANTED) {
@@ -326,7 +229,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //@Override
     public void onRequestPermissionResult(int requestCode, @NonNull String[] permission, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permission, grantResults);
         switch (requestCode) {
